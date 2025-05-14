15c60bb10358bae67b6040eb80ad225bf67ddb47
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-05-14 14:44:45 -0400

    Time: 15 ms (10.04%), Space: 24.2 MB (13.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -9,7 +9,7 @@ class Solution:
             buckets[freq].append(num)
 
         res = []
-        for i in range(len(nums), 0, -1):
+        for i in range(len(buckets) - 1, -1, -1):
             for num in buckets[i]:
                 res.append(num)
                 if len(res) == k:
