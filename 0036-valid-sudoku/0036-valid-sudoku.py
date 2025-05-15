b67e70a0f828e12147975663530800712f6ecde7
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-05-14 23:22:21 -0400

    Time: 3 ms (83.6%), Space: 17.9 MB (25.01%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,16 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        boxes = defaultdict(set)
+        seen = set()
 
         for row in range(9):
             for col in range(9):
                 if board[row][col] != '.':
-                    if board[row][col] in rows[row] or board[row][col] in cols[col] or board[row][col] in boxes[(row // 3, col // 3)]:
-                        return False
-                    rows[row].add(board[row][col])
-                    cols[col].add(board[row][col])
-                    boxes[(row // 3, col // 3)].add(board[row][col])
+                    rKey = (board[row][col], "row", row)
+                    cKey = (board[row][col], "col", col)
+                    bKey = (board[row][col], "box", row // 3, col // 3)
 
+                    if rKey in seen or cKey in seen or bKey in seen:
+                        return False
+
+                    seen.add(rKey)
+                    seen.add(cKey)
+                    seen.add(bKey)
         return True
