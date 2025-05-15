6a866da25003f74a83e900522e62e2e45bb41f0d
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-05-14 23:12:09 -0400

    Time: 0 ms (100%), Space: 18 MB (25.01%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,34 +1,16 @@
 class Solution:
-    def checkBox(self, board, row, col) -> bool:
-        nums = set()
-        for i in range(row, row+3):
-            for j in range(col, col+3):
-                if board[i][j] != '.':
-                    if board[i][j] in nums:
-                        return False
-                    nums.add(board[i][j])
-        return True
-
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+
         for row in range(9):
-            nums = set()
             for col in range(9):
                 if board[row][col] != '.':
-                    if board[row][col] in nums:
+                    if board[row][col] in rows[row] or board[row][col] in cols[col] or board[row][col] in boxes[(row // 3, col // 3)]:
                         return False
-                    nums.add(board[row][col])
-        
-        for col in range(9):
-            nums = set()
-            for row in range(9):
-                if board[row][col] != '.':
-                    if board[row][col] in nums:
-                        return False
-                    nums.add(board[row][col])
-            
-        for row in range(3):
-            for col in range(3):
-                if self.checkBox(board, row * 3, col * 3) == False:
-                    return False
+                    rows[row].add(board[row][col])
+                    cols[col].add(board[row][col])
+                    boxes[(row // 3, col // 3)].add(board[row][col])
 
         return True
