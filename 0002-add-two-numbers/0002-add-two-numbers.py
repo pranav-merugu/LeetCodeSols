c8028b1087ce7828c754cf9244e45cf65f264438
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-07-05 16:34:52 -0400

    Time: 4 ms (55.61%), Space: 17.7 MB (97.49%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,39 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        stack1 = []
-        while l1:
-            stack1.append(l1.val)
-            l1 = l1.next
-        stack2 = []
-        while l2:
-            stack2.append(l2.val)
-            l2 = l2.next
-        
-        dummy = ListNode(None)
+        dummy = ListNode(0, None)
         temp = dummy
-        carry = 0
-        while stack1 and stack2:
-            sum = stack1.pop(0) + stack2.pop(0) + carry
-            carry = sum // 10
+
+        total = carry = 0
+
+        while l1 or l2 or carry:
+            sum = carry
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            carry = sum //10
             temp.next = ListNode(sum % 10, None)
             temp = temp.next
-        
-        if stack1:
-            while stack1:
-                sum = stack1.pop(0) + carry
-                carry = sum // 10
-                temp.next = ListNode(sum % 10, None)
-                temp = temp.next
-        elif stack2:
-            while stack2:
-                sum = stack2.pop(0) + carry
-                carry = sum // 10
-                temp.next = ListNode(sum % 10, None)
-                temp = temp.next
-
-        if carry != 0:
-            temp.next = ListNode(carry, None)
         
         return dummy.next
 
