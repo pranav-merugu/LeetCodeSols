14eb1f9f4709b64d4ad38f6d37c0d4e5ae2f751b
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-09-12 22:18:14 -0400

    Time: 6 ms (78%), Space: 21.7 MB (15.49%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,15 +2,14 @@ from heapq import heapify, heappush, heappop
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda i : i[0])
-        result = [intervals[0]]
+        intervals.sort(key = lambda i : i[0]) #sort all intervals by their first element since we want to merge from 0 -> greatest start
+        result = []
 
-        for start, end in intervals[1:]:
-            prevEnd = result[-1][1]
-            if start <= prevEnd:
-                result[-1][1] = max(prevEnd, end)
-            else:
+        for start, end in intervals:
+            if len(result) == 0 or result[-1][1] < start: #if it does not overlap or there's no previous interal, just add the current interval
                 result.append([start, end])
+            else: #if it does overlap, merge the two
+                result[-1][1] = max(result[-1][1], end)
         return result
 
 
