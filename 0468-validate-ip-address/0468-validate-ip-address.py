98e8307cbe1a189e74060ff037c22e13d77ce23a
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-09-14 21:38:58 -0400

    Time: 0 ms (100%), Space: 17.8 MB (74.33%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.py
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -1,24 +1,24 @@
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
         validHex = {"a", "b", "c", "d", "e", "f", "A", "B", "C", "D", "E", "F"}
-        if ":" in queryIP:
-            sections = queryIP.split(":")
-        else:
-            sections = queryIP.split(".")
-        if len(sections) == 4:
-            for sec in sections:
-                for char in sec:
-                    if not char.isnumeric():
-                        return "Neither"
-                if len(sec) == 0 or (sec != '0' and sec[0] == '0') or int(sec) < 0 or int(sec) > 255:
+        ip4 = queryIP.split('.')
+        ip6 = queryIP.split(':')
+        if len(ip4) == 4:
+            for i in ip4:
+                if not i.isdigit():
+                    return "Neither"
+                if len(i) > 3 or (i != '0' and i[0] == '0'):
+                    return "Neither"
+                if not (0 <= int(i) <= 255):
                     return "Neither"
             return "IPv4"
-        elif len(sections) == 8:
-            for sec in sections:
-                if len(sec) < 1 or len(sec) > 4:
+
+        if len(ip6) == 8:
+            for i in ip6:
+                if not (1 <= len(i) <= 4):
                     return "Neither"
-                for char in sec:
-                    if not char.isnumeric() and char not in validHex:
+                for c in i:
+                    if not c.isdigit() and c not in validHex:
                         return "Neither"
             return "IPv6"
         else:
