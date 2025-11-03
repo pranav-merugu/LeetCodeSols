89a22ec1014b47da751f85028fa5369c4e06cc18
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-02 22:13:09 -0500

    Time: 11 ms (56.03%), Space: 26 MB (17.8%) - LeetHub

--- 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
+++ 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
@@ -9,17 +9,15 @@ class WordDistance:
         
 
     def shortest(self, word1: str, word2: str) -> int:
-        idx1 = 0
-        idx2 = 0
-        minDistance = 30001
-        while idx1 < len(self.wordIdx[word1]) and idx2 < len(self.wordIdx[word2]):
-            minDistance = min(minDistance, abs(self.wordIdx[word1][idx1] - self.wordIdx[word2][idx2]))
-            if idx1 + 1 < len(self.wordIdx[word1]) and abs(self.wordIdx[word1][idx1+1] - self.wordIdx[word2][idx2]) < minDistance:
+        idx1, idx2 = 0, 0
+        indexes1 = self.wordIdx[word1]
+        indexes2 = self.wordIdx[word2]
+        minDistance = float('inf')
+        while idx1 < len(indexes1) and idx2 < len(indexes2):
+            minDistance = min(minDistance, abs(indexes1[idx1] - indexes2[idx2]))
+            if indexes1[idx1] <= indexes2[idx2]:
                 idx1 += 1
-            elif idx2 + 1 < len(self.wordIdx[word2]) and abs(self.wordIdx[word1][idx1] - self.wordIdx[word2][idx2+1]) < minDistance:
-                idx2 += 1
             else:
-                idx1 += 1
                 idx2 += 1
         
         return minDistance
