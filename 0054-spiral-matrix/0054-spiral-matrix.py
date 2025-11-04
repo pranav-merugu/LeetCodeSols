5939f9f0fabcbcf3b3aa80123c9536f5561ac5df
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-03 22:10:22 -0500

    Time: 0 ms (100%), Space: 17.7 MB (61.69%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,24 +1,5 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-
-        def outer_loop(srow, scol, erow, ecol):
-            for i in range(scol, ecol):
-                if not 0 <= i < cols or len(res) >= rows * cols:
-                    break
-                res.append(matrix[srow][i])
-            for i in range(srow + 1, erow):
-                if not 0 <= i < rows or len(res) >= rows * cols:
-                    break
-                res.append(matrix[i][ecol - 1])
-            for i in range(ecol - 2, scol - 1, -1):
-                if (not 0 <= i < cols) or len(res) >= rows * cols:
-                    break
-                res.append(matrix[erow - 1][i])
-            for i in range(erow - 2, srow, -1):
-                if not 0 <= i < rows or len(res) >= rows * cols:
-                    break
-                res.append(matrix[i][scol])
-
         res = []
         srow, erow = 0, len(matrix) - 1
         scol, ecol = 0, len(matrix[0]) - 1
