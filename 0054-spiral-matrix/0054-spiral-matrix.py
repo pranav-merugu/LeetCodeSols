28d4538aa2067a8621b9fc17feff79aba6b052d9
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-03 22:09:00 -0500

    Time: 0 ms (100%), Space: 17.7 MB (87.92%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,7 +1,5 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        res = []
-        rows, cols = len(matrix), len(matrix[0])
 
         def outer_loop(srow, scol, erow, ecol):
             for i in range(scol, ecol):
@@ -20,14 +18,27 @@ class Solution:
                 if not 0 <= i < rows or len(res) >= rows * cols:
                     break
                 res.append(matrix[i][scol])
-                
-        srow, erow = 0, rows
-        scol, ecol = 0, cols
-        while srow < erow and scol < ecol:
-            outer_loop(srow, scol, erow, ecol)
+
+        res = []
+        srow, erow = 0, len(matrix) - 1
+        scol, ecol = 0, len(matrix[0]) - 1
+        while srow <= erow and scol <= ecol:
+            for i in range(scol, ecol + 1):
+                res.append(matrix[srow][i])
             srow += 1
-            scol += 1
-            erow -= 1
+
+            for i in range(srow, erow + 1):
+                res.append(matrix[i][ecol])
             ecol -= 1
-        
+
+            if srow <= erow:
+                for i in range(ecol, scol - 1, -1):
+                    res.append(matrix[erow][i])
+                erow -= 1
+
+            if scol <= ecol:
+                for i in range(erow, srow - 1, -1):
+                    res.append(matrix[i][scol])
+                scol += 1
+            
         return res
