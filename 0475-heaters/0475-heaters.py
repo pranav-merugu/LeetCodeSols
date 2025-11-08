b84b8adddaa63b509ce78c27baf2a19c4e8f6676
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-08 13:00:52 -0500

    Time: 36 ms (68.97%), Space: 20.9 MB (9.67%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,19 @@
+from bisect import bisect_left, bisect_right
+
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        m = len(heaters)
+        houses.sort()
+        heaters.sort()
+        minDistances = []
+        for house in houses:
+            minD = float('inf')
+            checkIdx = bisect_left(heaters, house)
+            if 0 <= checkIdx < m:
+                if abs(house - heaters[checkIdx]) < minD:
+                    minD = abs(house - heaters[checkIdx])
+            if 0 <= checkIdx - 1 < m:
+                if abs(house - heaters[checkIdx - 1]) < minD:
+                    minD = abs(house - heaters[checkIdx - 1])
+            minDistances.append(minD)
+        return max(minDistances)
