0b9505c56d71a5185d6c5e549624ddaeed94b51c
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-09 16:23:08 -0500

    Time: 191 ms (62.03%), Space: 19.4 MB (41.97%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -9,23 +9,18 @@ class Solution:
         if n == 1:
             return 1
 
-        queue = deque()
-        queue.append((0, 0, 1))
+        queue = deque([(0, 0, 1)])
         directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        minLength = float('inf')
-        visited = set()
-        visited.add((0, 0))
+        visited = {(0, 0)}
         while queue:
-            cur = queue.popleft()
+            r, c, d = queue.popleft()
+            if r == n - 1 and c == n - 1:
+                return d
 
-            for dl, dr in directions:
-                if 0 <= cur[0] + dl < n and 0 <= cur[1] + dr < n and (cur[0] + dl, cur[1] + dr) not in visited and grid[cur[0] + dl][cur[1] + dr] == 0:
-                    if cur[0] + dl == n - 1 and cur[1] + dr == n - 1:
-                        minLength = min(minLength, cur[2] + 1)
-                    queue.append((cur[0] + dl, cur[1] + dr, cur[2] + 1))
-                    visited.add((cur[0] + dl, cur[1] + dr))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < n and (nr, nc) not in visited and grid[nr][nc] == 0:
+                    queue.append((nr, nc, d + 1))
+                    visited.add((nr, nc))
         
-        if minLength == float('inf'):
-            return -1
-        else:
-            return minLength
+        return -1
