a36799ad245de8e08c201dc54423e2e86b3571d8
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-08 20:44:24 -0500

    Time: 0 ms (100%), Space: 17.8 MB (65.45%) - LeetHub

--- 1081-video-stitching/1081-video-stitching.py
+++ 1081-video-stitching/1081-video-stitching.py
@@ -1,6 +1,8 @@
 class Solution:
     def videoStitching(self, clips: List[List[int]], time: int) -> int:
         clips.sort()
+        '''
+        Solution 1: Greedy
         
         endTime = 0
         numClips = 0
@@ -19,6 +21,19 @@ class Solution:
             numClips += 1
         
         return numClips
-            
+        '''
+
+        dp = [float('inf')] * 101 #some intervals can have a time that exceeds the given time, so to be safe have the DP be the max possible size
+        dp[0] = 0
+
+        for start, end in clips:
+            for i in range(start, end + 1):
+                dp[i] = min(dp[i], dp[start] + 1)
+        
+        if dp[time] == float('inf'):
+            return -1
+        else:
+            return dp[time]
+
 
         
