7defeba218fcbbc4f2d29b49d01b9250cb3c98f2
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-10 01:02:03 -0500

    Time: 121 ms (40.01%), Space: 32.7 MB (39.12%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,13 @@
+from collections import deque
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
-        stack = []
-        for i, temp in enumerate(temperatures):
-            while stack and temp > stack[-1][0]:
-                item, index = stack.pop()
-                res[index] = i - index
-            stack.append([temp, i])
+        stack = deque([])
+        for idx, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][1]:
+                prevIdx, prevTemp = stack.pop()
+                res[prevIdx] = idx - prevIdx
+            stack.append((idx, temp))
         
         return res
-
