f50781c7bad69b0c6aa326f3d4136bac39f419ef
pranav-merugu <70034005+pranav-merugu@users.noreply.github.com>
2025-11-18 14:13:57 -0500

    Time: 35 ms (69.89%), Space: 22 MB (41.02%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+
+        inf = 2**31 - 1
+        #DP/BFS maybe, starting from gates and updating distance outwards
+        rows, cols = len(rooms), len(rooms[0])
+        gates = []
+        for row in range(rows):
+            for col in range(cols):
+                if rooms[row][col] == 0:
+                    gates.append((row, col, 0))
+
+        q = deque()
+        q.extend(gates)
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
+
+        while q:
+            row, col, dist = q.popleft()
+            for dr, dc in directions:
+                if 0 <= row + dr < rows and 0 <= col + dc < cols and rooms[row + dr][col + dc] > 0 and dist + 1 < rooms[row + dr][col + dc]:
+                    rooms[row + dr][col + dc] = dist + 1
+                    q.append((row + dr, col + dc, dist + 1))
+
+
+        
